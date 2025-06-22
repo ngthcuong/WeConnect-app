e6ed5c2a215e29ffa9a676a2ef98015821ca8aa4
ngthcuong <nguyenthanh8a2@gmail.com>
2025-06-22 15:49:33 +0700

    add: ModalProvider.jsx in context

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import HomePage from "@pages/HomePage";
-import { ThemeProvider } from '@emotion/react';
-import theme from "@/configs/mui.config"
+import HomePage from '@pages/HomePage'
+import { ThemeProvider } from '@emotion/react'
+import theme from '@/configs/mui.config'
+import { createBrowserRouter } from 'react-router-dom'
+import RootLayout from 'pages/RootLayout'
+import ModalProvider from 'context/ModalProvider'
+
+const router = createBrowserRouter([
+    {
+        element: <RootLayout />,
+        children: [
+            {
+                path: '/',
+                element: <HomePage />,
+            },
+        ],
+    },
+])
 
 createRoot(document.getElementById('root')).render(
-  <ThemeProvider theme={theme}> {/*Sử dụng theme của MUI được custom */}
-    <StrictMode>
-      <HomePage />
-    </StrictMode>
-  </ThemeProvider>,
+    //Sử dụng theme của MUI được custom
+    <ThemeProvider theme={theme}>
+        <ModalProvider>
+            <RouterProvider router={router} />
+        </ModalProvider>
+    </ThemeProvider>,
 )
