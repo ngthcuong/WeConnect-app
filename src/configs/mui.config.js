d23bdec441edfbebca386310ed79d294c14987ec
ngthcuong <nguyenthanh8a2@gmail.com>
2025-06-26 15:35:02 +0700

    chore: config breakpoint MUI and Tailwindcss

--- src/configs/mui.config.js
+++ src/configs/mui.config.js
@@ -1,13 +1,23 @@
-import { createTheme } from '@mui/material'
+import { createTheme } from "@mui/material";
 
 // https://mui.com/customization/palette/
 // Custom lại primary color
 const theme = {
-    palette: {
-        primary: {
-            main: '#246AA3',
-        },
+  palette: {
+    primary: {
+      main: "#246AA3",
     },
-}
+  },
+  breakpoints: {
+    values: {
+      xs: 0,
+      sm: 640,
+      md: 768,
+      lg: 1024,
+      xl: 1280,
+      "2xl": 1536,
+    },
+  },
+};
 
-export default createTheme(theme)
+export default createTheme(theme);
