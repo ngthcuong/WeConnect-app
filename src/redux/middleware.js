6888dc033d3d375708285c91c108897b24f16db8
ngthcuong <nguyenthanh8a2@gmail.com>
2025-07-04 01:29:46 +0700

    fix: clear data API when logout

--- src/redux/middleware.js
+++ src/redux/middleware.js
@@ -1,10 +1,12 @@
+import { rootApi } from "@services/rootApi";
 import { logout } from "./slices/authSlice";
 import { persistor } from "./store";
 
-export const logoutMiddleware = () => {
+export const logoutMiddleware = (store) => {
   return (next) => {
     return (action) => {
       if (action.type === logout.type) {
+        store.dispatch(rootApi.util.resetApiState());
         persistor.purge();
       }
       return next(action);
