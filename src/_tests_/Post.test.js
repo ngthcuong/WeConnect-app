128a366cee7e93ab408759fb82477a4e9bdf88d9
ngthcuong <nguyenthanh8a2@gmail.com>
2025-07-05 10:09:56 +0700

    test: enhance Post component tests for like functionality and rendering

--- src/_tests_/Post.test.js
+++ src/_tests_/Post.test.js
@@ -1,5 +1,5 @@
 import { default as Post } from "@components/NewsFeed/Post";
-import { render } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 
 describe("Post Component", () => {
   test("should render post component", () => {
@@ -11,5 +11,35 @@ describe("Post Component", () => {
       />,
     );
     expect(getByText("Hello World")).toBeInTheDocument();
+  });
+
+  test("display correct number of likes", () => {
+    const { getByText } = render(
+      <Post
+        likes={[1, 2, 3, 4]}
+        content={"Hello World"}
+        fullName={"John Doe"}
+        createdAt={new Date().toISOString()}
+      />,
+    );
+    expect(getByText("4")).toBeInTheDocument();
+  });
+
+  test("run onLike function with id when click on Like button", () => {
+    const mockOnLike = jest.fn();
+    const tempId = crypto.randomUUID();
+    render(
+      <Post
+        id={tempId}
+        content={"Hello World"}
+        fullName={"John Doe"}
+        createdAt={new Date().toISOString()}
+        onLike={mockOnLike}
+      />,
+    );
+
+    const likeButton = screen.getByText("Like");
+    fireEvent.click(likeButton);
+    expect(mockOnLike).toHaveBeenCalledWith(tempId);
   });
 });
